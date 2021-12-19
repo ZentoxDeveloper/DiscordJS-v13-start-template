504f1f42b4d2ce6ec4d8fbfaea5256d76fe22ded
Mr.Dragon <71612890+ZentoxDeveloper@users.noreply.github.com>
2021-12-19 01:38:16 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,16 +1,3 @@
-/*
-  _   _                  ____        _     ____                          _                    _ 
- | | | |_   _ _ __   ___| __ )  ___ | |_  |  _ \ ___ _ __ ___   __ _ ___| |_ ___ _ __ ___  __| |
- | |_| | | | | '_ \ / _ \  _ \ / _ \| __| | |_) / _ \ '_ ` _ \ / _` / __| __/ _ \ '__/ _ \/ _` |
- |  _  | |_| | |_) |  __/ |_) | (_) | |_  |  _ <  __/ | | | | | (_| \__ \ ||  __/ | |  __/ (_| |
- |_| |_|\__, | .__/ \___|____/ \___/ \__| |_| \_\___|_| |_| |_|\__,_|___/\__\___|_|  \___|\__,_|
-        |___/|_|                                                                                
-            
-We have tried our very hardest to make this the #1 Discord bot around to date! With tons of new
-features that not even some of the current bots around have! As-well with the new updated frame-
-work on DiscordJS V13! We have hopefull mastered this new land, and we plan to keep it that way!
-*/
-
 const { MessageEmbed } = require('discord.js');
 
 exports.run = async (client, message, args, con) => {
@@ -34,3 +21,11 @@ exports.info = {
     description: "See if im alive!",
     aliases: ['bing']
 }
+
+/**
+ * @INFO
+ * This bot is coded by @Mr. Dragon#4025 | https://discord.gg/VjyejfwPcm
+ * @INFO
+ * Work for ZentoxDevelopment | https://development.zentox.net
+ * @INFO
+ */
